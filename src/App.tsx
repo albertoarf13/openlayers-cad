8089d5f76ac7f0b280a06a28543293a4a840b4ae
Alberto Rivas <rivaalberto@gmail.com>
2026-06-20 08:59:57 -0600

    create open layers map

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
+import { useEffect, useRef } from 'react'
+import Map from 'ol/Map'
+import View from 'ol/View'
+import TileLayer from 'ol/layer/Tile'
+import OSM from 'ol/source/OSM'
+import 'ol/ol.css'
 import './App.css'
 
 function App() {
+  
+  const mapRef = useRef<HTMLDivElement>(null)
 
-  return (
-    <>
+  useEffect(() => {
+    if (!mapRef.current) return
 
-    </>
-  )
+    const map = new Map({
+      target: mapRef.current,
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 2,
+      }),
+    })
+
+    return () => map.setTarget(undefined)
+  }, [])
+
+  return <div ref={mapRef} className="map" />
 }
 
 export default App
